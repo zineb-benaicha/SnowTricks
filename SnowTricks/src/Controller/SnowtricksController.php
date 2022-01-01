a8f19affcd7fb6eb0070bcdc7501cde030e152a8
zineb benaicha <zineb.mezlef@gmail.com>
2022-01-01 10:51:12 +0100

    show figure list and delete option for the figure in the figure liste

--- SnowTricks/src/Controller/SnowtricksController.php
+++ SnowTricks/src/Controller/SnowtricksController.php
@@ -2,12 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Figure;
+use App\Repository\FigureRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class SnowtricksController extends AbstractController
 {
+    /*private ManagerRegistry $doctrine;
+
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }*/
+
     /**
      * @Route("/snowtricks", name="snowtricks")
      */
@@ -21,8 +34,35 @@ class SnowtricksController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function home()
+    public function home(FigureRepository $repo)
     {
-        return $this->render('snowtricks/home.html.twig');
+        $figuresList = $repo->findAll();
+        return $this->render('snowtricks/home.html.twig',[
+            'figuresList' => $figuresList
+        ]);
+    }
+
+    /**
+     * @Route("/figure/{id}", name="figure_show")
+     */
+    public function showFigure(Figure $figure)
+    {
+        return $this->render('snowtricks/show_figure.html.twig',[
+            'figure' => $figure
+        ]);
+    }
+
+    /**
+     * @Route("/figure/{id}/delete", name="figure_delete")
+     * @return RedirectResponse
+     */
+    public function deleteFigure(Figure $figure, ManagerRegistry $doctrine): RedirectResponse
+    {
+        $manager = $doctrine->getManager();
+        $manager->remove($figure);
+        $manager->flush();
+        return $this->redirectToRoute("/");
+
+
     }
 }
